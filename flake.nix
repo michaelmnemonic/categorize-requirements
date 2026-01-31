acf93a69172e14ea254c02d6cb7b0b33378943af
michaelmnemonic <michaelmnemonic@posteo.eu>
2026-01-31 16:42:20 +0100

    Refactor out setfit package

--- flake.nix
+++ flake.nix
@@ -3,11 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    setfit = {
+      url = "github:michaelmnemonic/setfit-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    setfit,
   }: let
     # Define 'forAllSystems' for properties that shall be build for x86_64 *and* aarch64
     systems = [
@@ -17,29 +22,6 @@
     forAllSystems = nixpkgs.lib.genAttrs systems;
     pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
   in {
-    packages = forAllSystems (system: {
-      setfit = pkgs.${system}.python3Packages.buildPythonPackage rec {
-        pname = "setfit";
-        version = "1.1.3";
-        pyproject = true;
-
-        src = pkgs.${system}.fetchPypi {
-          inherit pname;
-          inherit version;
-          hash = "sha256-21SntuhhGDQydcxww5dJ9x4QqiJv71EFQ7zXyLALnxk==";
-        };
-
-        dependencies = with pkgs.${system}.python313Packages; [
-          setuptools
-          datasets
-          sentence-transformers
-          evaluate
-          huggingface-hub
-          scikit-learn
-        ];
-      };
-    });
-
     devShells = forAllSystems (system: {
       default = pkgs.${system}.mkShell {
         buildInputs = with pkgs.${system}; [
@@ -51,7 +33,7 @@
           ruff
           python313Packages.datasets
           python313Packages.accelerate
-          self.packages.${system}.setfit
+          setfit.packages.${system}.setfit
 
           # nix
           nil
