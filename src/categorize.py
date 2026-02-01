6e929491d5ca337eb36cae9390d6c465c25e4e0a
michaelmnemonic <michaelmnemonic@posteo.eu>
2026-02-01 19:26:48 +0100

    Return category of input from cli

--- src/categorize.py
+++ src/categorize.py
@@ -2,53 +2,56 @@ import json
 from pathlib import Path
 from setfit import SetFitModel, SetFitTrainer, TrainingArguments, sample_dataset, Trainer
 from datasets import Dataset, load_dataset
+import argparse
 
 def main():
-    script_dir = Path(__file__).parent.resolve()
+    # Parse input arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--requirement', help='Requirement to categorize', type=str, required=True)
+    args = parser.parse_args()
+    requirement = args.requirement
 
+    # Load model
+    script_dir = Path(__file__).parent.resolve()
     model = SetFitModel.from_pretrained(script_dir.parent / "model")
 
-    requirements = [
-        "The login page must block IP addresses after 5 failed attempts.",
-        "The video stream delay should be less than 50 milliseconds.",
-        "If the cooling system fails, the reactor must shut down automatically."
-    ]
-
-    preds = model.predict(requirements)
-    probs = model.predict_proba(requirements)
+    # Determine category
+    preds = model.predict(requirement)
     print(preds)
 
-    print("-" * 30)
-    print("CATEGORIZATION RESULTS:")
-    print("-" * 30)
+    #probs = model.predict_proba(requirement)
 
-    preds = model.predict(new_requirements)
+    # print("-" * 30)
+    # print("CATEGORIZATION RESULTS:")
+    # print("-" * 30)
+
+    # preds = model.predict(new_requirements)
     
-    scores = probs[0].tolist()
-    results = []
+    # scores = probs[0].tolist()
+    # results = []
 
-    for label_id, score in enumerate(scores):
-        results.append({
-            "category": category_map[label_id],
-            "score": score
-        })
+    # for label_id, score in enumerate(scores):
+    #     results.append({
+    #         "category": category_map[label_id],
+    #         "score": score
+    #     })
 
-    # Sort by score (highest first)
-    results.sort(key=lambda x: x["score"], reverse=True)
+    # # Sort by score (highest first)
+    # results.sort(key=lambda x: x["score"], reverse=True)
 
-    # Single choice
-    for req, label_id in zip(new_requirements, preds):
-        category_name = category_map[int(label_id)]
-        print(f"Requirement: '{req}'")
-        print(f"-> Category: **{category_name}**\n")
+    # # Single choice
+    # for req, label_id in zip(new_requirements, preds):
+    #     category_name = category_map[int(label_id)]
+    #     print(f"Requirement: '{req}'")
+    #     print(f"-> Category: **{category_name}**\n")
 
-    # Multiple choice
-    print(f"Requirement: '{new_requirements[0]}'\n")
-    print("Matches:")
-    for res in results:
-        # Convert score to percentage
-        percentage = res['score'] * 100
-        print(f"- {res['category']}: {percentage:.1f}%")
+    # # Multiple choice
+    # print(f"Requirement: '{new_requirements[0]}'\n")
+    # print("Matches:")
+    # for res in results:
+    #     # Convert score to percentage
+    #     percentage = res['score'] * 100
+    #     print(f"- {res['category']}: {percentage:.1f}%")
 
 if __name__ == "__main__":
     main()
